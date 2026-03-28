794c77b333a4070c834f54e88ffba0671ea72e14
sirpmj <SIRPMJ@GMAIL.COM>
2026-03-28 18:17:18 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
-const CACHE_NAME = "tanker-app-v1";
+const CACHE_NAME = "reminder-app-v1";
+
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  "/",
+  "/index.html",
+  "/login.html",
 ];
 
-self.addEventListener("install", event => {
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("✅ Caching files");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+// Activate
+self.addEventListener("activate", (event) => {
+  console.log("✅ Service Worker Activated");
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
