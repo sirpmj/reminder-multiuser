de722f68f203c028bc1364e9d2fe8079691c08c6
sirpmj <SIRPMJ@GMAIL.COM>
2026-03-28 17:26:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,22 @@
-const CACHE_NAME = "Reminder-app-v12";   // 🔥 version change important
-
+const CACHE_NAME = "tanker-app-v1";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./login.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
-// INSTALL
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match("./index.html");
       })
   );
 });
