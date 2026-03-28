eca9176504d422e3f708090079b396075c70d8fe
sirpmj <SIRPMJ@GMAIL.COM>
2026-03-28 18:29:48 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,22 @@
-const CACHE_NAME = "reminder-app-v1";
+const CACHE_NAME = "reminder-app-v2";
 
 const urlsToCache = [
   "/",
   "/index.html",
   "/login.html",
+  "/manifest.json"
 ];
 
 // Install
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("✅ Caching files");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate
-self.addEventListener("activate", (event) => {
-  console.log("✅ Service Worker Activated");
-});
-
-// Fetch (offline support)
-self.addEventListener("fetch", (event) => {
+// Fetch
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
